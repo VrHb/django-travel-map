0dc9c1a2a25c7bd8793ee4ef28dca8f3434f29f3
Victor Abdukarimov <viredmi@gmail.com>
2022-11-16 21:58:23 +0300

    Added model for images

--- places/models.py
+++ places/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Place(models.Model):
     title = models.CharField("Название места", max_length=200)
     description_short = models.CharField(
@@ -28,3 +29,26 @@ class Place(models.Model):
         blank=True,
         null=True
     )
+    
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    upload = models.ImageField(
+        verbose_name="Загрузить",
+        upload_to="static/images/",
+        null=True,
+        blank=True
+    ) 
+    place = models.ForeignKey(
+        Place,
+        verbose_name="Место",
+        related_name="images",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.id} {self.place.title}"
+
