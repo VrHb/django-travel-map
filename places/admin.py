f7c050a79e14fb81537ff9bba5542985d1668462
Victor Abdukarimov <viredmi@gmail.com>
2022-11-17 19:38:35 +0300

    Added upload image fields in place

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 
 from .models import Place, Image
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    readonly_fields = ("id", )
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = (
         "title",
     )
+    inlines = [
+        ImageInline,
+    ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
