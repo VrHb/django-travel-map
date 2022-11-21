3207c8ed85321f2c013300d9aa2ebbe78ccdc4ca
Victor Abdukarimov <viredmi@gmail.com>
2022-11-21 18:52:20 +0300

    Fixed static root dir

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -77,7 +77,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.getenv('DJANGO_STATIC_ROOT_PATH')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
