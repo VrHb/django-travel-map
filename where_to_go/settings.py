c5107a01edcbeca5ffc1b11b5db1d243c14ccb90
Victor Abdukarimov <viredmi@gmail.com>
2022-11-21 19:05:21 +0300

    Fixed static root dir

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -72,12 +72,11 @@ TEMPLATES = [
         },
     },
 ]
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+   ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
