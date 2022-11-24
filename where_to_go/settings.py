a60ec2ab01d6d8fd06497162d6a800523594f337
Victor Abdukarimov <viredmi@gmail.com>
2022-11-24 12:15:33 +0300

    Fixed secret key env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -18,12 +18,11 @@ env = Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY') 
+SECRET_KEY = env.str('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False) 
